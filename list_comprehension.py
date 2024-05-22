d5b59244516e3e61a837af2dcc419ba5ca683cca
prem <prem@yahoo.com>
2024-05-22 14:00:31 +0530

    for else finding in list comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,16 @@
+
+
+list1 = []
+list1 = [i  for i in range(10)  if i % 2 == 0 ]
+print (list1)
+
+list1 = []
+# same as above, but error. may be because of the else is 
+# treated as for ... else ? 
+# list1 = [i  for i in range(10)  if i % 2 == 0  else i]
+print(list1)
+
+
+list1 = []
+list1 = [i  if i % 2 == 0 else i for i in range(10) ]
+print (list1)
